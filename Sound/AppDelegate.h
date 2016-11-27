b227bea87e350b6f12c0fc0eff44f9dd6fe4c5c4
chen <chenheard@163.com>
2016-11-27 16:09:34 +0800

    Initial Commit

---
+++ Sound/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sound
+//
+//  Created by water on 16/11/27.
+//  Copyright © 2016年 water. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sound/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sound
+//
+//  Created by water on 16/11/27.
+//  Copyright © 2016年 water. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
